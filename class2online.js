95edc0d7e43bc317db49a8af1f2b55412a30b2a5
Yousuf0206 <yousuf_saleem@hotmail.com>
2024-03-22 01:43:19 +0500

    push my calculator to github

---
+++ class2online.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "num1",
+        type: "number",
+        message: "Enter  First number:"
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: " Enter Second number: "
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "Select any operator to perform command",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    }]);
+if (answer.operator === "addition") {
+    console.log(answer.num1 + answer.num2);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.num1 - answer.num2);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.num1 * answer.num2);
+}
+else if (answer.operator === "division") {
+    console.log(answer.num1 / answer.num2);
+}
